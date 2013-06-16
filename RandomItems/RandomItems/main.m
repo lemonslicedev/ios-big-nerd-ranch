bd227f39d867f2df26c749c56802426b5478bb4c
Yan Shcherbakov <yanshcherbakov@Yans-MacBook-Pro-2.local>
2013-06-16 16:15:40 -0400

    Created root project RandomItems.

---
+++ RandomItems/RandomItems/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by Yan Shcherbakov on 2013-06-16.
+//  Copyright (c) 2013 One Shot Apps. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
